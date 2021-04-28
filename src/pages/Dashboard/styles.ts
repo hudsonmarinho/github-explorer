e962192202d4bf46d5d192daa58337d25bafb000
Hudson Marinho <hudsonmarinhodev@gmail.com>
2021-04-28 01:42:58 -0300

    Create Dashboard page

--- src/pages/Dashboard/styles.ts
+++ src/pages/Dashboard/styles.ts
@@ -1,6 +1,114 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
+import { shade, rgba } from 'polished';
+
+interface FormProps {
+  hasError: boolean;
+}
 
 export const Title = styled.h1`
+  max-width: 450px;
+  margin-top: 80px;
+  line-height: 56px;
   font-size: 48px;
   color: #3a3a3a;
+`;
+
+export const Form = styled.form<FormProps>`
+  max-width: 700px;
+  margin-top: 40px;
+  display: flex;
+
+  input {
+    flex: 1;
+    height: 70px;
+    padding: 0 24px;
+    border: 0;
+    border-radius: 5px 0 0 5px;
+    color: #3a3a3a;
+    border: 2px solid #fff;
+    border-right: 0;
+    transition: border-color 0.2s;
+
+    ${props =>
+      props.hasError &&
+      css`
+        border-color: #c53030;
+      `}
+
+    &::placeholder {
+      color: #a8a8b3;
+    }
+  }
+
+  button {
+    width: 200px;
+    height: 70px;
+    border: 0;
+    background: #04d361;
+    border-radius: 0 5px 5px 0;
+    color: #fff;
+    font-weight: bold;
+    transition: background-color 0.2s;
+
+    &:hover {
+      background: ${shade(0.2, '#04d361')};
+    }
+  }
+`;
+
+export const Repositories = styled.div`
+  margin-top: 80px;
+  max-width: 700px;
+
+  a {
+    background: #fff;
+    border-radius: 5px;
+    width: 100%;
+    padding: 24px;
+    text-decoration: none;
+    display: flex;
+    align-items: center;
+    transition: transform 0.2s;
+    box-shadow: 0px 0px 15px ${rgba('#ccc', 1)};
+
+    & + a {
+      margin-top: 16px;
+    }
+
+    &:hover {
+      transform: translateX(10px);
+    }
+
+    img {
+      width: 64px;
+      height: 64px;
+      border-radius: 50%;
+    }
+
+    div {
+      margin: 0 16px;
+      flex: 1;
+
+      strong {
+        font-size: 20px;
+        color: #3d3d4d;
+      }
+
+      p {
+        margin-top: 4px;
+        font-size: 18px;
+        color: #a8a8b3;
+      }
+    }
+
+    svg {
+      margin-left: auto;
+      color: #cbcbd6;
+    }
+  }
+`;
+
+export const Error = styled.div`
+  margin-top: 10px;
+  color: #c53030;
 `;
